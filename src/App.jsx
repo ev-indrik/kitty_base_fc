540cbda9592dd5e6e1f1d1ead5475357cc42fb5b
ev-indrik <delfinmur@gmail.com>
2024-05-04 20:19:46 +0300

    useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
 
-  const [searchField, setSearchField] = useState('')
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
-    setSearchField(searchFieldString)
+    setSearchField(searchFieldString);
 
- 
-  return (
-    <div className="App">
-      <h1 className="app-title">Furry Kitty Monsters</h1>
-      <SearchBox
-        onChangeHandler={onSearchChange}
-        placeholder="🐈 search fur monsters"
-        className="monster_search-box"
-      />
+    const filteredMonsters = monsters.filter((it) => {
+      return it.name.toLowerCase().includes(searchField);
+    });
 
-      <CardsList monsters={filteredMonsters} />
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1 className="app-title">Furry Kitty Monsters</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="🐈 search fur monsters"
+          className="monster_search-box"
+        />
+
+        <CardsList monsters={filteredMonsters} />
+      </div>
+    );
+  };
 }
 
 export default App;
