18e67d97d95c5a252f64f9a171b28a4d600ed5cf
ev-indrik <delfinmur@gmail.com>
2024-05-06 13:17:27 +0300

    SearchBox code fixed

--- src/components/search_box/search_box_component.jsx
+++ src/components/search_box/search_box_component.jsx
@@ -1,14 +1,11 @@
 import "./search_box_component_style.css";
 
-const SearchBox = () => {
-  return (
-    <input
-      className={`search-box ${props.className}`}
-      type="search"
-      placeholder={placeholder}
-      onChange={onChangeHandler}
-    />
-  );
-};
-
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
 export default SearchBox;
